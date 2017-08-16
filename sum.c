9f71541505d980aa05ac736a7ad0cd165e5bf9d4
MoahammadSufyan <31011543+MoahammadSufyan@users.noreply.github.com>
2017-08-16 12:23:03 -0700

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+ int main (void)
+ {
+ 	int a,b;
+ 	printf("Enetr Your First Value: ");
+ 	scanf("%d",&a);
+ 	printf("Enter Your second Value: ");
+ 	scanf("%d",&b);
+ 	printf(" Your Result is = %d",a+b);
+ }
