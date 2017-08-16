195b9dbd8a0003876e4eee918a54a22c618c1cbb
MoahammadSufyan <31011543+MoahammadSufyan@users.noreply.github.com>
2017-08-16 12:22:03 -0700

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include <conio.h>
+int main (void)
+{
+	int number;
+	printf("Enter Your No Plz:");
+	scanf("%d",&number);
+	switch(number){
+	 case 2:
+	 	printf("You Enterd 2");
+	 	break;
+	 	case 4:
+	 	printf("You Entered 4");
+	 	break;
+	 	case 6:
+	 	printf("You Enter 6");
+	 	break;
+	 	case 8:
+	 	printf("You Enter 8");
+	 	break;
+	 	default:
+	 	printf(" You Enetr invalid Value Plz Enetr Valid value");
+	 	
+	}
+}
+	
