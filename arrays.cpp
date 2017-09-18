7483a2ecb2fe1d7380179d476a7783e18cfff1de
MoahammadSufyan <31011543+MoahammadSufyan@users.noreply.github.com>
2017-09-18 12:27:58 -0700

    Arrays

---
+++ arrays.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int age[5];
+
+age[0] = 4;
+age[1] = 12;
+age[2] = 18;
+age[3] = 22;
+age[4] = 28;
+ 
+ printf("%d \n", age[0]);
+ printf("%d \n", age[1]);
+ printf("%d \n", age[2]);
+ printf("%d \n", age[3]);
+ printf("%d \n", age[4]);
+}
